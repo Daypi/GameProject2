48727f07dea3271cf017a36f179b3267c92406b2
TIM <lickid117@gmail.com>
2015-05-04 07:11:43 +0200

    Tiling script
    #yolo

--- Project/Assets/Map/Scripts/TextureTiling.cs
+++ Project/Assets/Map/Scripts/TextureTiling.cs
@@ -41,6 +41,6 @@ public class TextureTiling : MonoBehaviour {
 		// Figure out texture-to-mesh width based on user set texture-to-mesh height
 		float textureToMeshX = ((float)this.texture.width/this.texture.height)*this.textureToMeshZ;
 		
-		gameObject.GetComponent<Renderer>().material.mainTextureScale = new Vector2(planeSizeX*gameObject.transform.lossyScale.x/textureToMeshX, planeSizeZ*gameObject.transform.lossyScale.z/textureToMeshZ);
+		gameObject.GetComponent<Renderer>().sharedMaterial.mainTextureScale = new Vector2(planeSizeX*gameObject.transform.lossyScale.x/textureToMeshX, planeSizeZ*gameObject.transform.lossyScale.z/textureToMeshZ);
 	}
 }
