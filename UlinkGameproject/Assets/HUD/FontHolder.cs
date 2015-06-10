35c21e4237fef4c3f536558d475b690b8ce19f36
gambin_n <gambin_n@epitech.eu>
2015-06-11 03:12:18 +0900

    et je puuuusheyoooo

---
+++ UlinkGameproject/Assets/HUD/FontHolder.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FontHolder : MonoBehaviour {
+    public Font font;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- UlinkGameproject/Assets/HUD/HpBar.cs
+++ UlinkGameproject/Assets/HUD/HpBar.cs
@@ -8,12 +8,14 @@ public class HpBar : MonoBehaviour {
     public Texture2D maxHealthtex;
     public Texture2D currentHealthtex;
     public bool enabled;
+    Font font;
 
     void Start()
     {
 		if (Network.isServer == true)
 			this.enabled = false;
         camera = Camera.main;
+        font = GameObject.Find("PlayerHUD").GetComponent<FontHolder>().font;
     }
 
 	// Update is called once per frame
@@ -23,10 +25,11 @@ public class HpBar : MonoBehaviour {
             var centeredStyle = GUI.skin.GetStyle("Label");
             centeredStyle.alignment = TextAnchor.UpperCenter;
             centeredStyle.normal.textColor = Color.black;
+            centeredStyle.font = font;
             Vector3 onScreenPosition = camera.WorldToScreenPoint(this.transform.position);
             Rect displayRect = new Rect(onScreenPosition.x - 50, Screen.height - (onScreenPosition.y - 15), 100, 10);
-            Rect namePosition = new Rect(onScreenPosition.x - 50, Screen.height - (onScreenPosition.y +5), 100, 100);
-            Rect borderNamePosition = new Rect(onScreenPosition.x - 50 + 1, Screen.height - (onScreenPosition.y + 5) + 1, 100, 100);
+            Rect namePosition = new Rect(onScreenPosition.x - 50, Screen.height - (onScreenPosition.y -25), 100, 100);
+            Rect borderNamePosition = new Rect(onScreenPosition.x - 50 + 1, Screen.height - (onScreenPosition.y - 25) + 1, 100, 100);
             GUI.Label(borderNamePosition, pname);
             centeredStyle.normal.textColor = Color.yellow;
             GUI.Label(namePosition, pname);
