f86a3baa1e08fbed0ccf36fb0b2e60889ede4206
gambin_n <gambin_n@epitech.eu>
2015-06-10 23:28:27 +0900

    les balles !

---
+++ UlinkGameproject/Assets/HUD/BulletManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BulletManager : MonoBehaviour {
+    public int currentBullets = 0;
+    int maxBullets = 60;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int i = 1;
+	    while (i <= currentBullets)
+        {
+            GameObject.Find("PlayerHUD/" + i).GetComponent<Image>().enabled = true;
+            i++;
+        }
+        while (i <= maxBullets)
+        {
+            GameObject.Find("PlayerHUD/" + i).GetComponent<Image>().enabled = false;
+            i++;
+        }
+	}
+}
