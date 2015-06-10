6d798cae839a3b97e3fcda5fb89bff0f28a44bb8
gambin_n <gambin_n@epitech.eu>
2015-06-10 11:37:31 +0900

    les scores

---
+++ UlinkGameproject/Assets/ScoreBoard.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreBoard : MonoBehaviour {
+    public string scores;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scores = "";
+        scores += "Name \t   Kills \t Deaths \n";
+        ClientPlayer player = FindObjectOfType(typeof(ClientPlayer)) as ClientPlayer;
+        if (player)
+            scores += player.PlayerState.nickname + "\t\t" + player.PlayerState.NbKill + "\t\t" + player.PlayerState.NbDead + "\n";
+        ProxyPlayer[] proxies = FindObjectsOfType(typeof(ProxyPlayer)) as ProxyPlayer[];
+        foreach (var proxy in proxies)
+        {
+            scores += proxy.PlayerState.nickname + "\t\t" + proxy.PlayerState.NbKill + "\t\t" + proxy.PlayerState.NbDead + "\n";
+        }
+        this.GetComponent<Text>().text = scores;
+	}
+}
